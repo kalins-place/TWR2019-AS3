ae7f6c3ec6f7e8faece53b2458fd8ff1ca096d59
Kalin <61293134+kalins-place@users.noreply.github.com>
2026-07-24 22:14:17 -0400

    update page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,8 +27,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`K's ${siteConfig.title}`}
+      description="K's Docusaurus assignment meta description.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
