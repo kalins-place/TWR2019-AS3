faed06cb7d962c8b6e9cf67501db09bbc58810e9
Kalin <61293134+kalins-place@users.noreply.github.com>
2026-07-24 21:24:19 -0400

    docu config baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://kalins-place.github.io/TWR2019-AS3/',
+  url: 'https://kalins-place.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/TWR2019-AS3/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
